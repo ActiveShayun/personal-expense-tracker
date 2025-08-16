4044387108e6f2185c17cb10524a2db8b468585e
ActiveShayun <wevdeveloper0477gmail.com>
2025-08-17 01:50:32 +0600

    fixed cors issue

--- src/app/components/useAxiosHooks/useAxiosPublic.js
+++ src/app/components/useAxiosHooks/useAxiosPublic.js
@@ -2,7 +2,7 @@
 
 import axios from "axios"
 const instant = axios.create({
-    baseURL: process.env.NEXT_URL
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
 })
 
 export const useAxiosPublic = () => {
